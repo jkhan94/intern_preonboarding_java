1ddba9fb6bb829eea0919b50123138e0601570d5
jkhan94 <hjkwork@naver.com>
2024-09-03 17:05:38 +0900

    feat : 스프링 시큐리티와 필터 적용

---
+++ src/main/java/com/sparta/intern_preonboarding_java/common/config/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.sparta.intern_preonboarding_java.common.config;
+
+import com.sparta.intern_preonboarding_java.common.jwt.JwtProvider;
+import com.sparta.intern_preonboarding_java.common.security.AuthenticationEntryPointImpl;
+import com.sparta.intern_preonboarding_java.common.security.JwtAuthenticationFilter;
+import com.sparta.intern_preonboarding_java.common.security.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtProvider jwtProvider;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final AuthenticationEntryPointImpl authenticationEntryPoint;
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
+        return new JwtAuthenticationFilter(jwtProvider, userDetailsService);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.formLogin(AbstractHttpConfigurer::disable);
+
+        http.sessionManagement(
+                (sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+
+        http.authorizeHttpRequests(
+                (authorizationHttpRequests) -> authorizationHttpRequests
+                        .requestMatchers(HttpMethod.POST, "/api/auth/signup").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/auth/sign").permitAll()
+
+                        .anyRequest().authenticated()
+        );
+
+        http.exceptionHandling((e) ->
+                e.authenticationEntryPoint(authenticationEntryPoint));
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
